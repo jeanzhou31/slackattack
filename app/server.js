943f47f6f019de2786e109489319032fa653429e
jeanzhou31 <jeanzhou31@gmail.com>
2016-07-11 06:02:09 -0400

    trying gmaps

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 // example bot
 
 import botkit from 'botkit';
-const Yelp = require('yelp');
+import Yelp from 'yelp';
 
 console.log('starting bot');
 
@@ -87,19 +87,19 @@ controller.hears(['food', 'hungry', 'eat', 'restaurant'], ['direct_message', 'di
     ]);
   }
   function askFood(convo) {
-    convo.ask('What kind of food do you want?', (response) => {
+    convo.ask('What kind of food do you want?', (food) => {
       convo.say('Ok.');
-      askWhere(response, convo);
+      askWhere(food, convo);
       convo.next();
     });
   }
   function askWhere(food, convo) {
-    convo.ask('Where are you?', (response) => {
-      convo.say(`Ok! I can find ${food.text} in ${response.text}. One moment.`);
-      yelp.search({ term: `${food.text}`, location: `${response.text}` })
+    convo.ask('Where are you?', (place) => {
+      convo.say(`Ok! I can find ${food.text} in ${place.text}. One moment.`);
+      yelp.search({ term: `${food.text}`, location: `${place.text}` })
       .then((data) => {
         if (data.businesses.length === 0) {
-          convo.say(`Sorry! I couldn't find ${food.text} in ${response.text}!`);
+          convo.say(`Sorry! I couldn't find ${food.text} in ${place.text}!`);
         } else {
           const replyAttachment = {
             text: `rating: ${data.businesses[0].rating}`,
@@ -122,13 +122,61 @@ controller.hears(['food', 'hungry', 'eat', 'restaurant'], ['direct_message', 'di
         });
       })
       .catch((err) => {
-        convo.say(`Sorry! I couldn't find ${food.text} in ${response.text}.`);
+        convo.say(`Sorry! I couldn't find you location, ${place.text}.`);
       });
       convo.next();
     });
   }
   bot.startConversation(message, askYes);
 });
+
+// gmaps
+controller.hears(['map', 'direction', 'google', 'from'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+  function askYes(response, convo) {
+    convo.ask('Would you like map directions?', [
+      {
+        pattern: bot.utterances.yes,
+        callback: () => {
+          convo.say('Great! I will continue...');
+          askOrigin(convo);
+          convo.next();
+        },
+      },
+      {
+        pattern: bot.utterances.no,
+        callback: () => {
+          convo.say('Perhaps later.');
+          convo.next();
+        },
+      },
+      {
+        default: true,
+        callback: () => {
+          convo.say('Sorry, I\'m not sure what you\'re saying.');
+          convo.repeat();
+          convo.next();
+        },
+      },
+    ]);
+  }
+  function askOrigin(convo) {
+    convo.ask('Where is your origin?', (origin) => {
+      convo.say('Ok.');
+      askDestination(origin, convo);
+      convo.next();
+    });
+  }
+  function askDestination(origin, convo) {
+    convo.ask('Where is your destination?', (destination) => {
+      convo.say(`Ok! I will find directions from ${origin.text} to ${destination.text}. One moment.`);
+      const gmapsapi = process.env.GMAP_API_KEY;
+      const url = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin.text}&destination=${destination.text}&key=${gmapsapi}`;
+      convo.say(url);
+    });
+  }
+  bot.startConversation(message, askYes);
+});
+
 
 // otherwise, doesn't understand
 controller.hears('', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
